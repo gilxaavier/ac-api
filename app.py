a06ebc1807be7fb37404d75b30a3af1ff7131b9c
Gil Xavier <gilxaavier1@gmail.com>
2023-04-15 18:58:16 -0300

    API finalizada

--- app.py
+++ app.py
@@ -1,61 +1,44 @@
-from flask import Flask, jsonify, request
-import json
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
 desenvolvedores = [
     {"id": "0",
         "nome": "Gil Xavier",
-     "habilidades":["Python", "Flask"]},
+     "habilidades": ["Python", "Flask"]},
 
-    {"id" : "1",
+    {"id": "1",
         "nome": "Iury Assunção",
-     "habilidades":["JavaScript", "TypeScript"]},
+     "habilidades": ["JavaScript", "TypeScript"]},
     
     {"id": "2",
         "nome": "Alexandre Faria da Silva",
-    "habilidades": ["Desenvolvedor de APIs e MIcrosserviços", "Professor UNiversitario"]}
+        "habilidades": ["Desenvolvedor de APIs e Microsserviços", "Professor Universitário"]}
 ]
 
-@app.route("/dev/<int:id>/", methods= ["GET", "PUT","DELETE"])
-
-def desenvolvedor(id):
-    if request.method == "GET":
-        
-        try:
-            response = desenvolvedores[id]
-          
-        except IndexError:
-            mensagem = f"desenvolvedor de ID {id} não existe"
-            response = {"status": "erro", "mensagem" : mensagem} 
-        except Exception:
-            mensagem = "Erro desconhecido, Procure o administrador da API"
-            response = {"status": "erro", "mensagem" : mensagem} 
-        
-        return jsonify (response)
+# Buscar um desenvolvedor pelo ID
+@app.route("/dev/<int:id>/")
+def get_developer(id):
+    try:
+        response = desenvolvedores[id]
+        status_code = 200
+    except IndexError:
+        mensagem = f"Desenvolvedor de ID {id} não existe"
+        response = {"status": "erro", "mensagem": mensagem}
+        status_code = 404
+    except Exception as e:
+        mensagem = f"Erro desconhecido: {str(e)}"
+        response = {"status": "erro", "mensagem": mensagem}
+        status_code = 500
     
-    elif request.method == "PUT":
-        dados = json.loads(request.data)
-        desenvolvedores[id] = dados
-        return jsonify(dados)
-        
-    elif request.method == "DELETE":
-        desenvolvedores.pop(id)
-        return jsonify({"status": "sucesso!"}, {"mensagem" : "registro excluido!"})
+    return make_response(jsonify(response), status_code)
 
+#Listar todos os desenvolvedores
+@app.route("/dev/")
+def list_developers():
+    response = desenvolvedores
+    status_code = 200
+    return make_response(jsonify(response), status_code)
 
-#Lista todos os desenvolvedores e permite registrar um novo desenvolvedor
-@app.route("/dev/", methods=["POST", "GET"])
-def listDevelopers():
-    if request.method == "POST":
-        dados = json.loads(request.data)
-        position = len(desenvolvedores)
-        dados["id"] = position
-        desenvolvedores.append(dados)
-        return jsonify(desenvolvedores[position])
-        
-    elif request.method == "GET":
-        return jsonify(desenvolvedores)
-        
 if __name__ == "__main__":
     app.run(debug=True)

0a03fb50cc18d2640c6f46211506ccabdaeab550
Gil Xavier <gilxaavier1@gmail.com>
2023-04-17 20:57:44 -0300

    ultima alteracao

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -8,24 +8,28 @@ desenvolvedores = [
      "habilidades": ["Python", "Flask"]},
 
     {"id": "1",
-        "nome": "Iury Assunção",
+        "nome": "Iury Assuncao",
      "habilidades": ["JavaScript", "TypeScript"]},
     
     {"id": "2",
         "nome": "Alexandre Faria da Silva",
-        "habilidades": ["Desenvolvedor de APIs e Microsserviços", "Professor Universitário"]}
+        "habilidades": ["Desenvolvedor de APIs e Microsservicos", "Professor Universitario"]}
 ]
 
+@app.route("/")
+def helloWorld():
+    return ("Hello World")
+
 # Buscar um desenvolvedor pelo ID
-@app.route("/dev/<int:id>/")
+@app.route("/dev/<int:id>/", methods=["GET"])
 def get_developer(id):
     try:
         response = desenvolvedores[id]
         status_code = 200
     except IndexError:
-        mensagem = f"Desenvolvedor de ID {id} não existe"
-        response = {"status": "erro", "mensagem": mensagem}
+        mensagem = f"Desenvolvedor de ID {id} nao existe"
         status_code = 404
+        response = {"status": "erro", "mensagem": mensagem, "Codigo de erro": status_code}
     except Exception as e:
         mensagem = f"Erro desconhecido: {str(e)}"
         response = {"status": "erro", "mensagem": mensagem}
@@ -34,7 +38,7 @@ def get_developer(id):
     return make_response(jsonify(response), status_code)
 
 #Listar todos os desenvolvedores
-@app.route("/dev/")
+@app.route("/dev/", methods=["GET"])
 def list_developers():
     response = desenvolvedores
     status_code = 200
